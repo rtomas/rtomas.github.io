e149eb67c54b3d6facba73b14c762381987230ef
Tomas R <rtomas@gmail.com>
2016-09-03 18:21:41 -0300

    mejoras visuales

--- arm/sketch.js
+++ arm/sketch.js
@@ -1,5 +1,6 @@
 var capture, img;
 var start, startTime, limpio;
+var colorBackground;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
@@ -10,12 +11,12 @@ function setup() {
 	limpio = false;
 
 	img = loadImage("data/matom2.png"); 
+	colorBackground = color(255);
 }
 
 function draw() {
 	println(start);
 	if (start){
-		background(255);
 		var heightCamera = width*240/320;
 		var widthCamera = width;
 		if (heightCamera > height){
@@ -23,8 +24,11 @@ function draw() {
 			widthCamera = height*320/240;
 		}
 		imageMode(CENTER);
+
+		background(colorBackground);
+ 		
  		image(capture, width/2, height/2, widthCamera, heightCamera);
- 		image(img, width/2, height/2, widthCamera, heightCamera);
+ 		image(img, width/2, height/2+5, widthCamera, heightCamera);
 
  		Comienzo();
 	} else {
@@ -48,32 +52,80 @@ function Comienzo(){
 	var dif = millis()-startTime;
 	textSize(15);
 	fill(255);
+	noStroke();
 	if (dif >= 0 && dif <=4000){
-		fill(0);
-		text("< BLANCO >", width/2, 14);
+		colorBackground = color(0);
+
 		filter('INVERT');
+		fill(255);
+		rect(0,0,width,17);
+		rect(0,height-17,width,17);
+		fill(0);
+		text("COLOR: BLANCO", width/2, 14);
 	} else if (dif > 4000 && dif <=8000){
-		text("< ROSA >", width/2, 14);
+		colorBackground = color(238, 15, 183);
 		tint(238, 15, 183);
+
+		fill(238, 15, 183);
+		rect(0,0,width,17);
+		rect(0,height-17,width,17);
+		fill(0);
+		text("COLOR: ROSA", width/2, 14);
 	} else if (dif > 8000 && dif <=12000){
-		text("< GRIS >", width/2, 14);
+		colorBackground = color(127, 127, 127);
 		filter(GRAY);
 		tint(127, 127, 127);
+
+		fill(127, 127, 127);
+		rect(0,0,width,17);
+		rect(0,height-17,width,17);
+		fill(0);
+		text("COLOR: GRIS", width/2, 14);
 	} else if (dif > 12000 && dif <=16000){
-		text("< Naranja >", width/2, 14);
+		colorBackground = color(238, 178, 15);
 		tint(238, 178, 15);
+
+		fill(238, 178, 15);
+		rect(0,0,width,17);
+		rect(0,height-17,width,17);
+		fill(0);
+		text("COLOR: NARANJA", width/2, 14);
 	} else if (dif > 16000 && dif <=20000){
-		text("< Azul Indigo >", width/2, 14);
+		colorBackground = color(15, 104, 238);
 		tint(15, 104, 238);
+
+		fill(15, 104, 238);
+		rect(0,0,width,17);
+		rect(0,height-17,width,17);
+		fill(0);
+		text("COLOR: AZUL INDIGO", width/2, 14);
 	} else if (dif > 20000 && dif <=24000){
-		text("< Verde Oscuro >", width/2, 14);
+		colorBackground = color(31, 103, 29);
 		tint(31, 103, 29);
+
+		fill(31, 103, 29);
+		rect(0,0,width,17);
+		rect(0,height-17,width,17);
+		fill(0);
+		text("COLOR: VERDE OSCURO", width/2, 14);
 	} else if (dif > 24000 && dif <=28000){
-		text("< Lila >", width/2, 14);
+		colorBackground = color(179, 84, 219);
 		tint(179, 84, 219);
+
+		fill(179, 84, 219);
+		rect(0,0,width,17);
+		rect(0,height-17,width,17);
+		fill(0);
+		text("COLOR: LILA", width/2, 14);
 	} else if (dif > 28000 && dif <=32000){
-		text("< Verde Claro >", width/2, 14);
+		colorBackground = color(170, 220, 118);
 		tint(170, 220, 118);
+
+		fill(170, 220, 118);
+		rect(0,0,width,17);
+		rect(0,height-17,width,17);
+		fill(0);
+		text("COLOR: VERDE CLARO", width/2, 14);
 	} else if (dif > 32000)	{
 		start = false;
 		limpio = true;
